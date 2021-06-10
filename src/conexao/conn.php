204ba147a25505a3b6da5144fe4bd1d456b8c8f3
Yuri-sc <yurisantiagodacosta@gmail.com>
2021-06-09 23:00:18 -0300

    commit ajustes pasta libs

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -2,15 +2,15 @@
 
 // Declarar as variaveis necessarias para gerar a minha conexão com o Banco de Dados...
 
-$hostname = "sql108.epizy.com";
-$dbname = "epiz_28839504_XXX";
+$hostname = "sql108.byetcluster.com";
+$dbname = "epiz_28839504_library";
 $username = "epiz_28839504";
 $password = "ptBdtnNrkKUDLR0";
 
 try {
     $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo 'Conexão realizada com sucesso!';
+    echo 'Conexão realizada com sucesso!';
 } catch (PDOException $e) {
     echo 'Error: '.$e->getMessage();
 }
